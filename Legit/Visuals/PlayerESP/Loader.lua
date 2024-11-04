ce23768f7512a3ba679cfe22c9ed7914c9a96edb
dev <79853619+devvedbydev@users.noreply.github.com>
2024-11-04 17:31:55 +0000

    Update Loader.lua

--- Legit/Visuals/PlayerESP/Loader.lua
+++ Legit/Visuals/PlayerESP/Loader.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet(""))()
 loadstring(game:HttpGet(""))()
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Reconnect-Fallen/refs/heads/main/Legit/Visuals/PlayerESP/Skeletons.lua"))()
