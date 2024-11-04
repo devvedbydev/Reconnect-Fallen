b458211fe91ec00e2fb1b92502f7f90e12c57c7d
dev <79853619+devvedbydev@users.noreply.github.com>
2024-11-04 17:32:37 +0000

    Update Loader.lua

--- Legit/Visuals/PlayerESP/Loader.lua
+++ Legit/Visuals/PlayerESP/Loader.lua
@@ -1,3 +1,4 @@
+--// Player ESP
 loadstring(game:HttpGet(""))()
-loadstring(game:HttpGet(""))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Reconnect-Fallen/refs/heads/main/Legit/Visuals/PlayerESP/Skeletons.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Reconnect-Fallen/refs/heads/main/Legit/Visuals/PlayerESP/Names.lua"))() --// Name ESP
+loadstring(game:HttpGet("https://raw.githubusercontent.com/devvedbydev/Reconnect-Fallen/refs/heads/main/Legit/Visuals/PlayerESP/Skeletons.lua"))() --// Skeleton ESP
