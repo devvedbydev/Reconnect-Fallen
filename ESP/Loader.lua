e0552093bf4935da1207d6c7c1eee5738ae76361
dev <79853619+devvedbydev@users.noreply.github.com>
2024-11-04 17:29:05 +0000

    Delete ESP directory

--- ESP/Loader.lua
+++
@@ -1,3 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/devvedbydev/Reconnect/refs/heads/main/ESP/Health.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/devvedbydev/Reconnect/refs/heads/main/ESP/Skeleton.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/devvedbydev/Reconnect/refs/heads/main/ESP/Names.lua'))()
